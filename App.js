77329bd142295064b24cc1dae67a44a34ff04ac3
Siddhesh <sidd.mhapankar7@gmail.com>
2023-11-26 01:01:13 +0530

    updated commit

--- App.js
+++ App.js
@@ -2,13 +2,20 @@ import React, {useState} from 'react';
 import { Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
-  const [text, setText] = useState('');
+  function goalInputHandler(){
+
+  }
+  function addGoalHandler(){
+    
+  }
   return (
     <View className="flex-1 ">
-      <View className="flex flex-row justify-center mt-10 ">
-        <TextInput className="h-20 p-2 mx-2 border border-gray-200 border-1"
-        defaultValue="You can type in me"/>
-        <Button className="justify-center items-center" title='Add Goal'/>
+      <View className="w-full flex flex-row justify-center items-center pb-5 border-0 border-b border-b-gray-400 mt-20 ">
+        <TextInput className="h-12 w-3/5 pl-4 mx-2 border border-gray-200 border-1"
+        placeholder="Type your goal"
+        onChangeText={goalInputHandler}
+        />
+        <Button className="" title='Add Goal'/>
       </View>
       <View className="flex flex-row justify-center mt-10 ">
       <Text>List of Goals</Text>
