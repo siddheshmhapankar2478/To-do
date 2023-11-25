7fe03aa0ffd7b3f7a503d12dbcd3f62a83b5a2ab
Siddhesh <sidd.mhapankar7@gmail.com>
2023-11-26 00:01:05 +0530

    first commit

--- App.js
+++ App.js
@@ -8,10 +8,10 @@ export default function App() {
       <View className="flex flex-row justify-center mt-10 ">
         <TextInput className="h-20 p-2 mx-2 border border-gray-200 border-1"
         defaultValue="You can type in me"/>
-        <Button title='Add Goal'/>
+        <Button className="justify-center items-center" title='Add Goal'/>
       </View>
       <View className="flex flex-row justify-center mt-10 ">
-      <Button title='Add Goal'/>
+      <Text>List of Goals</Text>
       </View>
     </View>
   );
